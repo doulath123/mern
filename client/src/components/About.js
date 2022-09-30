816a20818152126e1dc281bc7a5fde9f0a242bc6
NAYAB SYED DOULATH BASHA <96289292+doulath123@users.noreply.github.com>
2022-09-30 18:38:04 +0530

    about page updated

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,10 +1,41 @@
-import React from 'react'
-
+import React, {useEffect,useState} from 'react'
+import {useNavigate} from "react-router-dom"
 function About() {
+
+    const [userData,setUserData]=useState({});
+
+    const navigate=useNavigate();
+    const callAboutPage=async ()=>{
+        try{
+            const res= await fetch('/about', {
+                method:'GET',
+                headers:{
+                    Accept:"application/json",
+                    "Content-Type":"application/json"
+                },
+                credentials:'include'
+            });
+            const data=await res.json();
+            setUserData(data);
+            if(!res.status===200){
+                const error=new Error(res.error);
+                throw error;
+            }
+
+
+        }catch(err){
+            console.log( "doubahi",err)
+            navigate("/signin")
+
+        }
+    }
+    useEffect(() => {
+        callAboutPage();
+    }, [])
   return (
    <>
    <div className='container emp-profile'>
-    <form method=''>
+    <form method='GET'>
         <div className='row'>
             <div className='col-md-4'>
                 <div className='profile-img'>
@@ -15,8 +46,8 @@ function About() {
 
             <div className='col-md-6'>
                 <div className='profil-head'>
-                    <h5>Doulath Basha</h5>
-                    <h6>Web Developer</h6>
+                    <h5>{userData.name}</h5>
+                    <h6>{userData.work}</h6>
                     <p className='profil-rating mt-3 mb-5'>RANKINGS: <span>1/10</span></p>
 
                     <ul className="nav nav-tabs" role="tablist">
@@ -58,7 +89,7 @@ function About() {
                                 <label >Name</label>
                             </div>
                             <div className='col-md-6'>
-                                <p>Doulath Basha</p>
+                                <p>{userData.name}</p>
                             </div>
                         </div>
                     </div>
